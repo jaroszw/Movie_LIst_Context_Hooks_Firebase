99a9d59a3a377bfd87e8b23dc5fa78159db8a69a
Wojciech Jarosz <jaroszw@gmail.com>
2020-11-24 13:40:16 +0100

    second

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,7 +2,7 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
-const firebaseConfig = {
+const config = {
   apiKey: "AIzaSyBeZWwmEwXFo9PTb_HEdJfTpic0wgjC10s",
   authDomain: "movie-list-app-d6d63.firebaseapp.com",
   databaseURL: "https://movie-list-app-d6d63.firebaseio.com",
@@ -12,20 +12,15 @@ const firebaseConfig = {
   appId: "1:294880965749:web:6dfa71ee02a3bbd315f43c",
 };
 
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(config);
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
 
-firebase
-  .auth()
-  .signInWithPopup(provider)
-  .then(function (result) {
-    var token = result.credential.accessToken;
-    var user = result.user;
-  })
-  .catch(function (error) {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    var email = error.email;
-    var credential = error.credential;
-  });
+export const signInWithGoogle = () => {
+  auth.signInWithPopup(provider);
+};
+
+export default firebase;
